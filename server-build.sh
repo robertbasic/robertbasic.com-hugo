77632dd22e3f11d14feffcf04a95ac9e608ceace
Robert Basic <robertbasic.com@gmail.com>
2022-11-14 22:52:56 +0100

    Server build bash script

---
+++ server-build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd /home/robert/robertbasic.com
+git fetch && git pull
+status=$(git status)
+if [[ "$status" == *"Your branch is up to date with 'origin/master'."* ]]; then
+    return 0
+fi
+
+./node_modules/.bin/postcss --env=production -o themes/robertbasic.com/static/css/main.css themes/robertbasic.com/static/css/tailwind/*.css
+./hugo
+
+rsync -rau /home/robert/robertbasic.com/public/ /var/www/robertbasic.com
